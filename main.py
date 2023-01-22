474346c3a04f94f830d3176c9ce013761671168c
Ronen Saviz <ronen@oversight.co.il>
2023-01-22 11:45:17 +0200

    write a priceCheck func

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def priceCheck(products, productPrices, productSold, soldPrice):
+    product_price_dict = dict(zip(products, productPrices))
+    errors = 0
+    for i in range(len(productSold)):
+        expected_price = product_price_dict[productSold[i]]
+        if expected_price != soldPrice[i]:
+            errors += 1
+    return errors
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    products = ['eggs', 'milk', 'cheese']
+    productPrices = [2.89, 3.29, 5.79]
+    productSold = ['eggs', 'eggs', 'cheese', 'milk']
+    soldPrice = [2.89, 2.99, 5.97, 3.29]
+    print(priceCheck(products, productPrices, productSold, soldPrice))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(priceCheck(
+        products=['rice', 'sugar', 'wheat', 'cheese'],
+        productPrices=[16.89, 56.92, 20.89, 345.99],
+        productSold=['rice', 'cheese'],
+        soldPrice=[18.99, 400.89]
+    ))
